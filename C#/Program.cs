d68f87063b887db6548993387df62d1e0851cb5d
BlissMahlathi <162612924+BlissMahlathi@users.noreply.github.com>
2024-09-25 13:01:20 +0200

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Model number: ");
+            int choice = Convert.ToInt32 (Console.ReadLine());
+            if(choice == 1)
+            {
+
+            
+                Console.WriteLine(" ");
+                Console.WriteLine("********Model By Hlulani mahlathi********");
+                Console.WriteLine("______________________________________");
+                Console.WriteLine("         |      EXTRUCTOR     |       ");
+                Console.WriteLine("        electron extration site       ");
+                Console.WriteLine("_________|____________________|_______");
+                Console.WriteLine("________________|_|_|_|_______________");
+                Console.WriteLine("________________|* * * |_______________             _____________________________________ _");
+                Console.WriteLine("|*    *    *    *    *    *    *    * |             |    1    |   2    |   3    |   4    | |");
+                Console.WriteLine("|  *    *    *    *    *    *    *    |-------------|         |        |        |        | |__________");
+                Console.WriteLine("|*    *    *    STORAGE   *    *    *  -> -> -> -> ->         C O N V E R T O R            | -> -> ->|");
+                Console.WriteLine("|  *    *    *  SYSTEM *    *    *    |-------------|    5    |   6    |   7    |   8    | |----| ** |");
+                Console.WriteLine("|_____________________________________|             |_________|________|________|________|_|    | ** |");
+                Console.WriteLine("                                                                                                | ** |");
+                Console.WriteLine("                                                                                                | ** |");
+                Console.WriteLine("                                                                                                | ** |");
+                Console.WriteLine("                                                                                                / ** \\");
+                Console.WriteLine("                                                                                               / * * *\\");
+                Console.WriteLine("                                                                             \\|||||||/       / * * * *\\");
+                Console.WriteLine("                                                                              ||||||||       | * * * * *|");
+                Console.WriteLine("                                                                              ||||||||       |    C     |");
+                Console.WriteLine("                                                                              |      |       |    U     |");
+                Console.WriteLine("                                                                              |      |       |    R     |");
+                Console.WriteLine("                                                                              |      |       |    R     |");
+                Console.WriteLine("                                                                              |      |       |    E     |");
+                Console.WriteLine("                                                                              |      |      /     N    /");
+                Console.WriteLine("                                                                              |      |_____/      T   /");
+                Console.WriteLine("                                                                              |                      /");
+                Console.WriteLine("                                                                              |                     /");
+                Console.WriteLine("                                                                              |____________________/");
+                Console.WriteLine("                                                                                                   ");
+                Console.ReadLine();
+            }
+            else{
+                Console.WriteLine("NOT AVAILABLE!!!");
+            }
+        }
+    }
+}
